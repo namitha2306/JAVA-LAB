5affc2c783b75232d1975a230414db39e38aa323
NAMITHA.M.S <namitha2002@pg.cusat.ac.in>
2022-05-25 10:13:58 +0530

    Sum
    sum

---
+++ Java day 1/Sum of two numbers/Ide (1).java
@@ -0,0 +1,15 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Add two numbers");
+    int first = 100;
+    int second = 25;
+    
+    System.out.println(first + " " + second);
+
+   
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
